0b8a0b69c538af91608c152d7da1711da595044c
Adam Sadler <adambsadler@gmail.com>
2024-11-06 10:06:50 -0500

    Initial Commit

---
+++ RecipeFetcher/RecipeFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFetcherApp.swift
+//  RecipeFetcher
+//
+//  Created by Adam Sadler on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
